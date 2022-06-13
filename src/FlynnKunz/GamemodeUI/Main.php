7d8b1cf2d507347b971dd6feba96134bdb7a5b07
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-06-13 02:28:14 -0500

    Add support FormsUI

--- src/FlynnKunz/GamemodeUI/Main.php
+++ src/FlynnKunz/GamemodeUI/Main.php
@@ -16,7 +16,7 @@ use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
-use FormAPI\{CustomForm, SimpleForm};
+use Vecnavium\FormsUI\SimpleForm;
 
 class Main extends PluginBase {
 	
