2e7d60e17844a9106fa22ce8acd46aba3d4ce270
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-06-13 03:16:14 -0500

    NEW SOUNDS

---
+++ src/FlynnKunz/GamemodeUI/utils/PluginUtils.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FlynnKunz\GamemodeUI\utils;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+
+use pocketmine\utils\TextFormat;
+
+use pocketmine\world\Position;
+use ReflectionClass;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+# PluginUtils by fernanACM
+
+class PluginUtils {
+
+	public static function PlaySound(Player $player, string $sound, $volume = 1, $pitch = 1) {
+		$packet = new PlaySoundPacket();
+		$packet->x = $player->getPosition()->getX();
+		$packet->y = $player->getPosition()->getY();
+		$packet->z = $player->getPosition()->getZ();
+		$packet->soundName = $sound;
+		$packet->volume = 1;
+		$packet->pitch = 1;
+		$player->getNetworkSession()->sendDataPacket($packet);
+	}
+}
